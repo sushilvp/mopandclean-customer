5080b2593071915a0a473572fb7fe3aadb36bbea
Anu <anumohanonline97@gmail.com>
2026-02-16 08:03:10 +0530

    testing error fixs

--- application/models/Job_model.php
+++ application/models/Job_model.php
@@ -19,10 +19,10 @@ class Job_model extends CI_Model {
 
     public function get_by_customer($customer_id)
     {
-        $this->db->select('jobs.*, service_type.name as service_name, job_status.name as status_name');
+        $this->db->select('jobs.*, service_type.name as service_name, status_type.name as status_name');
         $this->db->from('jobs');
         $this->db->join('service_type', 'service_type.id = jobs.service_id', 'left');
-        $this->db->join('job_status', 'job_status.id = jobs.status_id', 'left');
+        $this->db->join('status_type', 'status_type.id = jobs.status_id', 'left');
         $this->db->where('jobs.customer_id', $customer_id);
         $this->db->order_by('jobs.created_at', 'DESC');
         return $this->db->get()->result();
@@ -30,10 +30,10 @@ class Job_model extends CI_Model {
 
     public function get_active_by_customer($customer_id)
     {
-        $this->db->select('jobs.*, service_type.name as service_name, job_status.name as status_name');
+        $this->db->select('jobs.*, service_type.name as service_name, status_type.name as status_name');
         $this->db->from('jobs');
         $this->db->join('service_type', 'service_type.id = jobs.service_id', 'left');
-        $this->db->join('job_status', 'job_status.id = jobs.status_id', 'left');
+        $this->db->join('status_type', 'status_type.id = jobs.status_id', 'left');
         $this->db->where('jobs.customer_id', $customer_id);
         $this->db->where_in('jobs.status_id', [1, 2, 3]); // Pending, Confirmed, In Progress
         $this->db->order_by('jobs.created_at', 'DESC');
@@ -42,10 +42,10 @@ class Job_model extends CI_Model {
 
     public function get_past_by_customer($customer_id)
     {
-        $this->db->select('jobs.*, service_type.name as service_name, job_status.name as status_name');
+        $this->db->select('jobs.*, service_type.name as service_name, status_type.name as status_name');
         $this->db->from('jobs');
         $this->db->join('service_type', 'service_type.id = jobs.service_id', 'left');
-        $this->db->join('job_status', 'job_status.id = jobs.status_id', 'left');
+        $this->db->join('status_type', 'status_type.id = jobs.status_id', 'left');
         $this->db->where('jobs.customer_id', $customer_id);
         $this->db->where_in('jobs.status_id', [4, 5]); // Completed, Cancelled
         $this->db->order_by('jobs.created_at', 'DESC');
@@ -54,10 +54,10 @@ class Job_model extends CI_Model {
 
     public function get_by_id($id, $customer_id)
     {
-        $this->db->select('jobs.*, service_type.name as service_name, job_status.name as status_name');
+        $this->db->select('jobs.*, service_type.name as service_name, status_type.name as status_name');
         $this->db->from('jobs');
         $this->db->join('service_type', 'service_type.id = jobs.service_id', 'left');
-        $this->db->join('job_status', 'job_status.id = jobs.status_id', 'left');
+        $this->db->join('status_type', 'status_type.id = jobs.status_id', 'left');
         $this->db->where('jobs.id', $id);
         $this->db->where('jobs.customer_id', $customer_id);
         return $this->db->get()->row();
